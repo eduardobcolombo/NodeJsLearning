895b8d5b3e4668267f5ee7f83c04347a9a913e48
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-04-23 07:55:39 -0300

    Added Maps autocomplete and popup at points

--- routes/index.js
+++ routes/index.js
@@ -45,12 +45,14 @@ router.post('/account/reset/:token',
   catchErrors(authController.updatePassword)
 );
 
+router.get('/map', storeController.mapPage);
+
 /*
   API
 */
 
 router.get('/api/search', catchErrors(storeController.searchStores));
-
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 
 
