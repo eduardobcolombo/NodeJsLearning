a2c37a0bcc480f238c4972c6ee41b86e0e7a8d23
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-05-09 10:53:07 -0300

    Added the reviews on card and paginatino

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,15 +6,15 @@ const jimp = require('jimp');
 const uuid = require('uuid');
 
 const multerOptions = {
-    storage: multer.memoryStorage(),
-    fileFilter(req, file, next) {
-        const isPhoto = file.mimetype.startsWith('image/');
-        if (isPhoto) {
-            next(null, true);
-        } else {
-            next({ message: 'That filetype isn\'t allowed!' }, false);
-        }
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
     }
+  }
 };
 
 exports.homePage = (req, res) => {
@@ -50,8 +50,27 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-    const stores = await Store.find();
-    res.render('stores', {title: 'Stores', stores: stores});
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit) - limit;
+
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash('info', `Hey! You asked for page ${page}. But that doesn't exist. So I put you on page ${pages}`);
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+
+  res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
@@ -165,4 +184,10 @@ exports.heartStore = async (req, res) => {
             { new: true }
         );
     res.json(user);
+};
+
+exports.getTopStores = async (req, res) => {
+    const stores = await Store.getTopStores();
+    // res.json(stores);
+    res.render('topStores', {stores, title: '⭐ Top Stores!'});
 };
